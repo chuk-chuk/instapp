44ba34a9c7b4189b4b217603c0e9ec1decd366b3
chuk-chuk <yulia.v.litvin@gmail.com>
2017-08-28 17:04:47 +0100

    fixed an error from a post not being submitted

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-  belongs_to :user
+
+  belongs_to :user, optional: true
 
   has_attached_file :image, :styles => { :large => "600x600>", :medium => "300x300#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
