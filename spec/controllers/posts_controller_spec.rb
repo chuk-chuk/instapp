1ce950680eecb7bacdfd2668bad2b38191d755ab
chuk-chuk <yulia.v.litvin@gmail.com>
2017-09-02 15:27:36 +0100

    testing post controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
+require 'user_helpers'
 
 RSpec.describe PostsController, type: :controller do
+
+  describe 'GET' do
+    let!(:user) { create(:user) }
+    let!(:post) { create(:post) }
+
+    before do
+      login(user)
+    end
+
+    it "GET / responds with 200" do
+      get :index
+      expect(response).to have_http_status(200)
+    end
+
+  end
 
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  
+  let!(:post) { create(:post) }
+
+  it { is_expected.to be }
+
+ describe "#posted_at" do
+   it "responds to the #posted_at method" do
+     expect(post).to respond_to(:posted_at)
+   end
+ end
+
 end
